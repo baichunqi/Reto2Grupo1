fd189903437487d7cd4874883892f89260f9d292
MarkelDiez <markelm.diezm@gmail.com>
2023-12-18 15:56:04 +0100

    Añadido el activity de login y cambiadas las propiedades de loa imageView para que se puedan visualizar

--- app/src/main/java/com/example/reto2grupo1/MainActivity.kt
+++ app/src/main/java/com/example/reto2grupo1/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.reto2grupo1
 
+import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.view.View
@@ -8,6 +9,7 @@ import android.view.animation.Animation
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.reto2grupo1.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +26,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onAnimationEnd(animation: Animation) {
-                // Acciones al finalizar la animación (opcional)
+                IntentCall()
             }
 
             override fun onAnimationRepeat(p0: Animation?) {
@@ -39,6 +41,11 @@ class MainActivity : AppCompatActivity() {
         logoAnimation.setImageResource(R.drawable.logoanimation)
         val runningVessel = logoAnimation.drawable as AnimationDrawable
         runningVessel.start()
+    }
 
+    fun IntentCall(){
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
---
+++ app/src/main/java/com/example/reto2grupo1/ui/login/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.example.reto2grupo1.ui.login
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.reto2grupo1.R
+
+class LoginActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+}
